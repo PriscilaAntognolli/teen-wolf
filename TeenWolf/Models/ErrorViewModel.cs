7dd7e613838fda09806647c149e998b2ffbc351a
Priscila Antognolli <estrelaorion.22@gmail.com>
2023-04-10 08:56:51 -0300

    Feito as alterações e adicionado no projeto

--- TeenWolf/Models/ErrorViewModel.cs
+++ TeenWolf/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace TeenWolf.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
---
+++ TeenWolf/Models/Personagem.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeenWolf.Models
+{
+    public class Personagem
+    {
+        public int Numero { get; set; }
+        public string Nome { get; set; }
+        
+    }
+}
